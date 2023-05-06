838131c7762d4d50dd458ec64b38ec2c1cbc1b6a
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-05-06 23:01:59 +0200

    Add mobile adaptions

--- changelogs/script.js
+++ changelogs/script.js
@@ -14,4 +14,23 @@ function openVersion(id) {
     for (var i = 0; i < aside_versionlist_version.length; i++) aside_versionlist_version[i].classList.remove('active-version');
     document.getElementById("select-" + id).classList.add('active-version');
     changelogIframe.src = "./versions/" + id + ".html";
+    if (mobileMenuOpen) closeMobileMenu();
+}
+
+// 
+// Function: openMobileMenu()
+//
+var mobileMenuOpen = false;
+
+function openMobileMenu() {
+    document.querySelector('aside').style.zIndex = "10";
+    mobileMenuOpen = true;
+}
+
+//
+// Function: closeMobileMenu()
+//
+function closeMobileMenu() {
+    document.querySelector('aside').style.zIndex = "-500";
+    mobileMenuOpen = false;
 }
